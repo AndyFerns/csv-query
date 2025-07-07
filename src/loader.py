423f8cb04f37ba5ebae3422712f00ced6481b3b1
AndyFerns <write2andrew.fernandes@gmail.com>
2025-07-08 04:18:19 +0530

    summary stat method added

--- src/loader.py
+++ src/loader.py
@@ -12,3 +12,10 @@ def get_columns(filename):
     with open(filename, newline='', encoding='utf-8') as f:
         reader = csv.reader(f)
         return next(reader)
+
+def get_summary_stats(rows): #for distinct keyword
+    import pandas as pd
+    if not rows:
+        return "No data."
+    df = pd.DataFrame(rows)
+    return df.describe(include='all').to_string()
