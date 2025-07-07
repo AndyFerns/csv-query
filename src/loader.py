26379a622dfb1fed271b8445d81ba79dd39639f6
AndyFerns <write2andrew.fernandes@gmail.com>
2025-07-08 03:23:37 +0530

    added column retrieval for DESC

--- src/loader.py
+++ src/loader.py
@@ -1,11 +1,14 @@
 import csv
+import os
 
 def load_csv(filename):
-    try:
-        with open(filename, newline='', encoding='utf-8') as f:
-            reader = csv.DictReader(f)
-            return list(reader)
-    except FileNotFoundError:
+    if not os.path.exists(filename):
         raise FileNotFoundError(f"File '{filename}' not found.")
-    except Exception as e:
-        raise RuntimeError(f"Error reading CSV: {e}")
+    with open(filename, newline='', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        return list(reader)
+
+def get_columns(filename):
+    with open(filename, newline='', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        return next(reader)
